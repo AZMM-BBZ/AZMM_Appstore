4ffe35b47d4b970003b8e9a081f9aec91d1b6fc6
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2024-03-21 09:10:32 +0100

    Added Backend Project Froghopper.

---
+++ Froghopper/Froghopper/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace Froghopper.models
+{
+    [PrimaryKey(nameof(Pid))]
+    public class Person
+    {
+        public int Pid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
