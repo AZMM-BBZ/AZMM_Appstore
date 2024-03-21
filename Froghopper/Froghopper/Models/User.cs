616273e9a2ffe482d2322e4b1a98c40567b95439
RuvexEUW <155663661+RuvexEUW@users.noreply.github.com>
2024-03-21 09:29:46 +0100

    Delete ALL

--- Froghopper/Froghopper/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Froghopper.models
-{
-    public class User : Person
-    {
-        public List<App> OwendApps { get; set; } = new List<App>();
-        public double MoneySpent { get; set; }
-    }
-}
