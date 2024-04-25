bb7000e8acf313baca68b1bbefd14c01d691b4c7
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2024-04-25 09:34:53 +0200

    Added Authentication (missing Serivce)

--- AZMM/AZMM.Server/Controllers/AuthenticationController.cs
+++ AZMM/AZMM.Server/Controllers/AuthenticationController.cs
@@ -1,6 +1,10 @@
 ﻿using AZMM.Server.DtoModel;
+using Froghopper.models;
+using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
@@ -24,7 +28,33 @@ namespace AZMM.Server.Controllers
         [HttpPost("authenticate")]
         public ActionResult<string> Authenticate([FromBody] AuthenticationRequestBody authenticationRequestBody)
         {
-            return Ok();
+            var user = new User { Uid = 1, Name="test", Password="test", Role=new Models.Role { Rid = 1, RoleName = "User"} };//_authenticationService.ValidateUserCredentials(authenticationRequestBody.Username, authenticationRequestBody.Password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var securityKey =
+                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("userid", user.Uid.ToString()));
+            claimsForToken.Add(new Claim("username", user.Name));
+            claimsForToken.Add(new Claim("password", user.Password));
+            claimsForToken.Add(new Claim(JwtClaimTypes.Role, user.Role.RoleName));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(30),
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            _log.LogInformation("New JWT Token: " + tokenToReturn);
+            return Ok(tokenToReturn);
         }
     }
 }
