0a45c2857fdf049870ac535e0238572dbb4293e4
RuvexEUW <155663661+RuvexEUW@users.noreply.github.com>
2024-05-23 13:04:56 +0200

    Work and Games cards added

--- AZMM/azmm.client/src/Work.vue
+++ AZMM/azmm.client/src/Work.vue
@@ -1,16 +1,98 @@
 ﻿<template>
-    <header>
+    <br />
 
-    </header>
+    <div class="grid-container">
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/Notepad++_Logo.png" class="card-img-top" alt="notepad++ logo" style="width:100px;height:100px;">
 
-      <main>
+                <div class="card-body">
+                    <h5 class="card-title">Notepad++</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/Notepad++_Logo.png" class="card-img-top" alt="notepad++ logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler2</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/Notepad++_Logo.png" class="card-img-top" alt="notepad++ logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler3</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/Notepad++_Logo.png" class="card-img-top" alt="notepad++ logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler4</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
 
 
-          <section>
-              <p>This is just the filler for the Work page</p>
-          </section>
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/visual-studio-code4470.jpg" class="card-img-top" alt="VS-Code logo" style="width:100px;height:100px;">
 
-    </main>
+                <div class="card-body">
+                    <h5 class="card-title">VS-Code</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/visual-studio-code4470.jpg" class="card-img-top" alt="VS-Code logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler5</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/visual-studio-code4470.jpg" class="card-img-top" alt="VS-Code logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler6</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="width:200px;height:200px;">
+            <div class="card">
+                <img src="/src/assets/visual-studio-code4470.jpg" class="card-img-top" alt="VS-Code logo" style="width:100px;height:100px;">
+
+                <div class="card-body">
+                    <h5 class="card-title">Filler7</h5>
+                    <a href="#" class="btn btn-primary">Download</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script setup lang="ts">
@@ -18,5 +100,18 @@
 </script>
 
 <style scoped>
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto auto;
+        gap: 50px;
+        background-color: #ffffff;
+        padding: 50px;
+    }
 
+    .grid-container > div {
+        background-color: rgba(255, 255, 255, 0.8);
+        border: 0px solid black;
+        text-align: center;
+        font-size: 30px;
+    }
 </style>
