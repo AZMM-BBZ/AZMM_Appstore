e0120ad40db1c20ad605a693e8a37e2391e29a15
sinsir2 <157989693+sinsir2@users.noreply.github.com>
2024-06-06 11:39:29 +0200

    Added appservice

--- AZMM/AZMM.Server/Context/AzmmDbContext.cs
+++ AZMM/AZMM.Server/Context/AzmmDbContext.cs
@@ -44,12 +44,25 @@ namespace Froghopper.Context
             var user = new User { Uid = 2, Name = "user", Password = "user", Email = "test@test.com", RoleId = 1 };
 
 
-            var notepad = new App { Aid = 1, Name = "Notepad++", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/69/Notepad%2B%2B_Logo.svg", Price = 0, FileName= "npp.8.5.8.Installer.x64" };
-            var vsCode = new App { Aid = 2, Name = "VS Code", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/69/Notepad%2B%2B_Logo.svg", Price = 0, FileName = "VSCodeUserSetup-x64-1.88.1" };
-            var manicDigger = new App { Aid = 3, Name = "Manic Digger", Description = "Notepad", Category = Enums.Category.Game, ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/69/Notepad%2B%2B_Logo.svg", Price = 0, FileName = "ManicDigger2011-02-12Setup" };
-            var jp2 = new App { Aid = 4, Name = "JP2", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/6/69/Notepad%2B%2B_Logo.svg", Price = 0, FileName = "jp2setup" };
+            var notepad = new App { Aid = 1, Name = "Notepad++", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName= "npp.8.5.8.Installer.x64" };
+            var vsCode = new App { Aid = 2, Name = "VS Code", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "VSCodeUserSetup-x64-1.88.1" };
+            var manicDigger = new App { Aid = 3, Name = "Manic Digger", Description = "Notepad", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "ManicDigger2011-02-12Setup" };
+            var jp2 = new App { Aid = 4, Name = "JP2", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "jp2setup" };
+            var fallDies = new App { Aid = 5, Name = "Fall Dies", Description = "Fall guys but not fall guys", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "FallDies" };
+            var msLemonS = new App { Aid = 6, Name = "VS Code", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "MsLemonS" };
+            var mrTomatoS = new App { Aid = 7, Name = "Manic Digger", Description = "Notepad", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "MrTomatoS" };
+            var visualStudio = new App { Aid = 8, Name = "JP2", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "VisualStudioSetup (1)" };
+            var postman = new App { Aid = 9, Name = "Fall Dies", Description = "Fall guys but not fall guys", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "Postman-win64-Setup (1)" };
+            var office = new App { Aid = 10, Name = "VS Code", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "OfficeSetup" };
+            var fileZilla = new App { Aid = 11, Name = "Manic Digger", Description = "Notepad", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "FileZilla_3.66.0_win64_sponsored2-setup" };
+            var teams = new App { Aid = 12, Name = "JP2", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "Teams_windows_x64" };
+            var ditto = new App { Aid = 13, Name = "Fall Dies", Description = "Fall guys but not fall guys", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "DittoSetup_64bit_3_24_246_0 (1)" };
+            var mainFrame = new App { Aid = 14, Name = "VS Code", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "Mainframe Blaster" };
+            var minecraft = new App { Aid = 15, Name = "Manic Digger", Description = "Notepad", Category = Enums.Category.Game, ImageUrl = "", Price = 0, FileName = "MinecraftInstaller (1)" };
+            var yoshi = new App { Aid = 16, Name = "JP2", Description = "Notepad", Category = Enums.Category.Work, ImageUrl = "", Price = 0, FileName = "Yoshi" };
 
-            modelBuilder.Entity<App>().HasData(notepad, vsCode, manicDigger, jp2);
+            modelBuilder.Entity<App>().HasData(notepad, vsCode, manicDigger, jp2, fallDies, 
+                msLemonS, mrTomatoS, visualStudio, postman, office, fileZilla, teams, ditto, mainFrame, minecraft, yoshi);
             modelBuilder.Entity<Role>().HasData(adminRole, authorRole, userRole, elevatedAuthorRole);
             modelBuilder.Entity<User>().HasData(adminUser, user);
         }
